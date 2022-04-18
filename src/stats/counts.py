9c7886f3d0cd20fc01268a128bb145d09320e23f
limyuzheng88 <limyuzheng88@yahoo.com.sg>
2022-04-18 10:24:30 +0000

    1. updated Analysis.ipynb

--- src/stats/counts.py
+++ src/stats/counts.py
@@ -3,16 +3,6 @@ from collections import Counter
 import sys
 from tqdm import tqdm
 import json
-import magic
-
-# detect file encoding. If ANSI (magic can't identify), specify that its ANSI
-def detect_file_encoding(path):
-    blob = open(path, 'rb').read()
-    m = magic.Magic(mime_encoding=True)
-    encoding = m.from_buffer(blob)
-    if encoding=='unknown-8bit':
-        encoding='ANSI'
-    return encoding
 
 corpus = Path(sys.argv[1])
 outfile = Path(sys.argv[2])
@@ -24,8 +14,7 @@ for f in fs:
     name = f.stem
     counts[name] = {}
     
-    encoding = detect_file_encoding(f)
-    with open(f, encoding=encoding) as fp:
+    with open(f, encoding='utf-8') as fp:
         for line in tqdm(fp):
             hist = Counter(line.split())
             for k in hist:
